5b3e2b982e7611c0c52f92e7d5c796853f4dffec
Thiago Camargo <thiago.camargo@veon.com>
2018-06-19 11:52:59 +0200

    Extra juice

--- src/main/kotlin/com/xmppjingle/shogun/Shogun.kt
+++ src/main/kotlin/com/xmppjingle/shogun/Shogun.kt
@@ -8,7 +8,8 @@ class Shogun {
 
     companion object {
 
-        fun crunch(payload: String, minWl: Int, maxWl: Int, layers: Int, charset: Charset): Pair<String, HashMap<String, Char>> {
+        fun crunch(payload: String, minWl: Int, maxWl: Int, layers: Int, charset: Charset) = crunch(payload, minWl, maxWl, layers, charset, emptyList())
+        fun crunch(payload: String, minWl: Int, maxWl: Int, layers: Int, charset: Charset, opening: List<String>): Pair<String, HashMap<String, Char>> {
 
             println("Original Size: ${payload.length}")
 
@@ -16,14 +17,20 @@ class Shogun {
             var cr = payload
             for (i in 0..layers) {
 //            println(cr)
-                val ordered = slash(minWl, maxWl, layers, cr, charset)
+                var word: String
+                if (i < opening.size) {
+                    word = opening[i]
+                } else {
+                    val ordered = slash(minWl, maxWl, layers, cr, charset)
 //            ordered.forEach({ println(it) })
-                if (ordered.isEmpty()) break
-                val entry = ordered[0]
+                    if (ordered.isEmpty()) break
+                    val entry = ordered[0]
+                    word = entry.first
+                }
                 val rp = (257 + i).toChar()
 //                println("$entry for $rp")
-                dict.put(entry.first, rp)
-                cr = cr.replace(entry.first, "$rp", false)
+                dict.put(word, rp)
+                cr = cr.replace(word, "$rp", false)
             }
 
 //            println("{{$cr}}")
@@ -51,24 +58,24 @@ class Shogun {
 //                println("Breaking on $wl...")
                 if (wl >= payload.length) break
                 var i = validCut(payload.slice(0..(wl - 1)), encoder)
-                while (i < (payload.length - wl )) {
+                while (i < (payload.length - wl)) {
                     val word = payload.slice(i..(wl + i - 1))
                     val cut = validCut(word, encoder)
                     if (cut != 0) {
                         i += cut
-
-                    }else{
-                    if (encoder.canEncode(word[wl-1])) {
-                        t.computeIfPresent(word, { _, u -> u + wl + 2 })
-                        t.computeIfAbsent(word, { wl })
-//                        println(word)
-                        i++
                     } else {
-                        i += wl + 1
-                    }}
+                        if (encoder.canEncode(word[wl - 1])) {
+                            t.computeIfPresent(word, { _, u -> u + wl + 2 })
+                            t.computeIfAbsent(word, { wl })
+//                        println(word)
+                            i++
+                        } else {
+                            i += wl + 1
+                        }
+                    }
                 }
             }
-            val ordered = t.toList().sortedBy { (_, v) -> v }
+            val ordered = t.toList().sortedBy { (k, v) -> v /*+ (k.length * wordLenBonus)*/ }
             if (ordered.isEmpty()) return ordered
             return ordered.filter { it.second > it.first.length }.reversed()//.subList(0, if (ordered.size > top) top else ordered.size)
         }
@@ -78,7 +85,7 @@ class Shogun {
             var j = 0
             while (j < word.length) {
                 if (!encoder.canEncode(word[j])) {
-                    i = j +1
+                    i = j + 1
                 }
                 j++
             }
